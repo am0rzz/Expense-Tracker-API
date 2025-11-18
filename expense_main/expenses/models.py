ae523b9e4a2e0fbe0c56ef2362ed41814872853c
am0rzz <punish3r297@gmail.com>
2025-11-18 18:00:28 +0200

    (feat): Configure the Expense model.

--- expense_main/expenses/models.py
+++ expense_main/expenses/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator 
+from decimal import Decimal
 # Create your models here.
 
 
 class Expense(models.Model):
-    title = models.CharField(max_length=100)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField()
+    title = models.CharField(max_length=100, blank= False)
+    amount = models.DecimalField(max_digits=10, decimal_places=2,validators=[MinValueValidator(Decimal('0.01'))], blank=False)
+    description = models.TextField(blank=True)
     category_choices = {
         "GR" : "Groceries",
         "LE" : "Leisure",
@@ -17,3 +18,6 @@ class Expense(models.Model):
         "OT" : "Others"
     }
     category = models.CharField(max_length=2, choices=category_choices, default="OT")
+
+    def __str__(self):
+        return self.title
