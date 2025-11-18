621847c59270b0a585dcc3a3e164a2cb6f642e7c
am0rzz <punish3r297@gmail.com>
2025-11-18 17:38:58 +0200

    Add the Expense model with its attributes.

--- expenses/models.py
+++ expenses/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Expense(models.Model):
+    title = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    description = models.TextField()
+    category_choices = {
+        "GR" : "Groceries",
+        "LE" : "Leisure",
+        "EL" : "Electronics",
+        "UT" : "Utilities",
+        "CL" : "Clothing",
+        "HE" : "Health",
+        "OT" : "Others"
+    }
+    categories = ['Groceries', 'Leisure', 'Electronics', 'Utilities', 'Clothing', 'Health', 'Others']
+    category = models.CharField(max_length=2, choices=category_choices, default="OT")
