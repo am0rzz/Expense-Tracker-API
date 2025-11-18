5e157cea19c5839871371f2b0270042c89c5196f
am0rzz <punish3r297@gmail.com>
2025-11-18 23:32:58 +0200

    Add last name to required fields.

--- expense_main/users/models.py
+++ expense_main/users/models.py
@@ -1,20 +1,20 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
 from .managers import CustomUserManager
 # Create your models here.
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser,PermissionsMixin):
     email = models.EmailField(max_length=200, unique=True)
     username = models.CharField(max_length=50, unique=True)
     first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100, default= '')
+    last_name = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     date_of_birth = models.DateField()
 
-    REQUIRED_FIELDS = ['username', 'first_name','date_of_birth']
+    REQUIRED_FIELDS = ['username', 'first_name','last_name','date_of_birth']
     USERNAME_FIELD = "email"
 
     objects = CustomUserManager()
