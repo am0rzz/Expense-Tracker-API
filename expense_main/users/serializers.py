0723bbae36df1372937bbfe8a8c53cc6bfe4dc97
am0rzz <punish3r297@gmail.com>
2025-11-19 01:47:48 +0200

    Fix signupSerializer.

--- expense_main/users/serializers.py
+++ expense_main/users/serializers.py
@@ -1,9 +1,19 @@
 from rest_framework import serializers
+from django.contrib.auth import authenticate
 from .models import User
 
 class SignupSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['email','username','first_name','last_name','date_of_birth']
+        fields = ['email','username','first_name','last_name','date_of_birth','password']
         read_only_fields = ['is_staff','is_superuser','is_active']
+
+    def create(self, validated_data):
+        password = validated_data.pop("password")
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+
