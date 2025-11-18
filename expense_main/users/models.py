a9865979b1321b4c06c12f66cbc2d2b0e8dcd8fb
am0rzz <punish3r297@gmail.com>
2025-11-18 22:58:51 +0200

    Add custom user model.

---
+++ expense_main/users/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+
+from .managers import CustomUserManager
+# Create your models here.
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=200, unique=True)
+    username = models.CharField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100, default= '')
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    date_of_birth = models.DateField()
+
+    REQUIRED_FIELDS = ['username', 'first_name','date_of_birth']
+    USERNAME_FIELD = "email"
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
