bb8788f3408baad46d87b337a28432eb028fc8cc
am0rzz <punish3r297@gmail.com>
2025-11-19 18:46:02 +0200

    Configure Logout url view.

--- expense_main/users/urls.py
+++ expense_main/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import Register
+from .views import Register, LogOutView
 
 urlpatterns =[
     path('register/',Register.as_view()),
+    path('logout/', LogOutView.as_view())
 ]
